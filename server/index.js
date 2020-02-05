3ec800d21865927b55b31efddc1b52f86afb6fea
wuxubin <43275249+wuxubin@users.noreply.github.com>
2020-02-05 14:36:12 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require("express")
 
 const app = express()
 
-// app.set('secret', 'i2u34y12oi3u4y8')
+app.use(require('cors')())
+app.use(express.json())
+
+require('./plugins/db')(app)
+require('./routes/admin')(app)
 
 app.listen(3000, () => {
     console.log('http://localhost:3000');
