31d2a063e535da0dc395f7e221c36f76f032098f
wuxubin <43275249+wuxubin@users.noreply.github.com>
2020-02-07 11:59:23 +0800

    文章详情，登录校验

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -8,20 +8,28 @@ import CategoryList from '../views/CategoryList.vue'
 import DocumentList from '../views/DocumentList.vue'
 import ArticleEdit from '../views/ArticleEdit.vue'
 import ArticleList from '../views/ArticleList.vue'
+import Article from '../views/Article.vue'
 import HelpDocument from '../views/HelpDocument.vue'
-import Edit from '../views/Edit.vue'
+import Login from '../views/Login.vue'
+import AdminUserList from '../views/AdminUserList.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/login', name: 'Login', component: Login, meta: { isPublic: true } },
   {
     path: '/',
     name: 'main',
     component: Main,
+    redirect: 'manage/documents',
     children: [
-      { path: 'edit', component: Edit },
       { path: 'setting', component: Words },
-      { path: 'help/:id', component: HelpDocument, props: true },
+      {
+        path: 'help/:id', component: HelpDocument, props: true,
+        children: [
+          { path: ':article_id', component: Article, props: true, }
+        ]
+      },
       {
         path: 'manage',
         component: Manage,
@@ -29,6 +37,7 @@ const routes = [
         children: [
           { path: 'documents', component: DocumentList },
           { path: 'categories', component: CategoryList },
+          { path: 'users', component: AdminUserList },
           { path: 'articles/create', component: ArticleEdit },
           { path: 'articles/edit/:id', component: ArticleEdit, props: true },
           { path: 'articles/list', component: ArticleList },
@@ -41,5 +50,10 @@ const routes = [
 const router = new VueRouter({
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  if (!to.meta.isPublic && !localStorage.token) {
+    return next('/login')
+  }
+  next()
+})
 export default router
