0a7e28adb7241ceb548b0fa94ce7339b3bc424b6
wuxubin <43275249+wuxubin@users.noreply.github.com>
2020-02-10 21:11:53 +0800

    。。。

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ app.set('secret', 'sdDjkSXf;89e')
 app.use(require('cors')())
 app.use(express.json())
 
+app.use('/admin', express.static(__dirname + '/admin'))
+
 require('./plugins/db')(app)
 require('./routes/admin')(app)
 
 app.listen(3000, () => {
-    console.log('http://localhost:3000');
-  });
+  console.log('http://localhost:3000');
+});
