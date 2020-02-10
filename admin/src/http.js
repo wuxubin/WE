fa96618c7e40571d26d80c931d80075b6024aac5
wuxubin <43275249+wuxubin@users.noreply.github.com>
2020-02-10 14:16:36 +0800

    生产环境api

--- admin/src/http.js
+++ admin/src/http.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import router from './router'
 
 const http = axios.create({
-  baseURL: 'http://localhost:3000/admin/api'
+  baseURL: process.env.VUE_APP_API_URL || 'admin/api'
 })
 http.interceptors.request.use(function (config) {
   // Do something before request is sent
@@ -23,12 +23,12 @@ http.interceptors.response.use(res => {
       type: 'error',
       message: err.response.data.message
     })
-    
+
     if (err.response.status === 401) {
       router.push('/login')
     }
   }
-  
+
   return Promise.reject(err)
 })
 
